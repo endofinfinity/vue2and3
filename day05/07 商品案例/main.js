4d289e0f88e029692f71beb32a740454682eb739
mailab <z1563492018@gmail.com>
2023-09-28 22:13:30 +0800

    day05done

---
+++ day05/07 商品案例/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+Vue.config.productionTip = false
+
+Vue.directive('focus', {
+  inserted (el) {
+    el.focus()
+  }
+})
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
--- day05/day05demo/src/main.js
+++ day05/day05demo/src/main.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Find from './views/Find.vue'
+import My from './views/My.vue'
+import Friend from './views/Friend.vue'
 
+// 注册vue-router路由
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
-Vue.directive('focus', {
-  inserted (el) {
-    el.focus()
-  }
+// 创建路由实例对象
+const router = new VueRouter({
+  routes:[
+    // 路径，组件
+    { path:'/find',component:Find},
+    { path:'/my',component:My},
+    { path:'/friend',component:Friend},
+  ]
 })
 
 new Vue({
   render: h => h(App),
+  // 关联路由实例
+  router:router
 }).$mount('#app')
