dd67e45a6881443ae8d742a931f3495176e1ccad
mailab <z1563492018@gmail.com>
2023-10-20 23:00:26 +0800

    day06 04 路由重定向及404

--- day06/05传参-动态路由/router/index.js
+++ day06/05传参-动态路由/router/index.js
@@ -1,21 +1,15 @@
 import Home from '@/views/Home'
 import Search from '@/views/Search'
-import NotFound from '@/views/NotFound'
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter) // VueRouter插件初始化
 
 // 创建了一个路由对象
 const router = new VueRouter({
-	mode:'history',
   routes: [
-		
-		{path:'/',redirect:'/home'},
     { path: '/home', component: Home },
 		// ?表示可选
-    { path: '/search/:key?', component: Search },
-		{path:'*',component:NotFound},
+    { path: '/search/:key?', component: Search }
   ]
 })
 
