d7626130a8522a13ff33e06393d08284abe22152
mailab <z1563492018@gmail.com>
2025-08-12 00:01:40 +0800

    day06  vuex前小修

---
+++ day06/08 面经 接口503/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from '@/router/index.js'
+
+
+new Vue({
+  render: h => h(App),
+  // 3、关联路由实例
+  router:router
+}).$mount('#app')
